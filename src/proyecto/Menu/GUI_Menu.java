d8a7c0067f9b9c6ba3113a58c50bb3efaeacf61c
Elías <Elas@DESKTOP-2KETQI6>
2023-07-31 18:56:38 -1200

    ajuste de la cancion

--- src/proyecto/Menu/GUI_Menu.java
+++ src/proyecto/Menu/GUI_Menu.java
@@ -117,13 +117,11 @@ public class GUI_Menu extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
       musica = java.applet.Applet.newAudioClip(getClass().getResource
-        ("Zelda.wav")); 
+        ("/Music/Zelda.wav")); 
        musica.play();
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -148,7 +146,7 @@ public class GUI_Menu extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new GUI_Menu().setVisible(true);
