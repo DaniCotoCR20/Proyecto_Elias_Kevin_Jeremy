31512ddea58b2d2dfa2ca60fdbb24ed671ddb247
jerem <jerem@LAPTOP-E71OG2F9>
2023-08-01 20:28:41 -0600

    creacion de atributos y constructores de la clase NodoPila_ingrediente

--- src/Estructura_Pila_Ingredientes/Nodo_Pila_ingrediente.java
+++ src/Estructura_Pila_Ingredientes/Nodo_Pila_ingrediente.java
@@ -3,13 +3,14 @@ package Estructura_Pila_Ingredientes;
 
 import proyecto.Objetos.Ingrediente;
 
-//Atributos y constructores//
+//Atributos y constructores de la clase NodoPila_ingrediente//
 public class Nodo_Pila_ingrediente {
     
     private Ingrediente ingrediente;
     private Nodo_Pila_ingrediente siguiente;
 
-    public Nodo_Pila_ingrediente(Ingrediente ingrediente, Nodo_Pila_ingrediente siguiente) {
+    public Nodo_Pila_ingrediente(Ingrediente ingrediente, Nodo_Pila_ingrediente 
+            siguiente) {
         this.ingrediente = null;
         this.siguiente = null;
     }
