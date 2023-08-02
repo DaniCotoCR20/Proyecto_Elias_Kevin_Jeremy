b052e5fbb21abec0c6faaf6e1a4d977eb85fe17d
Elías <Elas@DESKTOP-2KETQI6>
2023-08-01 12:50:13 -1200

    Creacion de la clase Cola_orden, atributos, metodo que muestra el tamanio de la cola y el metodo vacia que me muestra si la orden esta vacia o no.

--- src/Estructura_Orden/Cola_Orden.java
+++ src/Estructura_Orden/Cola_Orden.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Estructura_Orden;
 
-/**
- *
- * @author Kevin
- */
+
 public class Cola_Orden {
+  
+     //Creacion de la clase orden y sus atributos//
+    
+    private NodoCola_Orden frente;
+    private NodoCola_Orden ultimo;
+    private int largo;
+
+    //Metodo que me mujestra el tamanio y el largo de la cola
+    
+    public int tamanio() {
+        return largo;
+    }
+
+    //Metodo que me muestra si la cola esta vacia o no//
+    
+    public boolean Vacia() {
+        return frente == null;
+    }
+    
     
 }
