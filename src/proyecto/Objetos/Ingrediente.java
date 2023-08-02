0147278d6b8574774fbc0ee14041460cbfd36a92
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 18:08:02 -0600

    Creacion de atributos y constructor de la clase Ingredientes.

--- src/proyecto/Objetos/Ingrediente.java
+++ src/proyecto/Objetos/Ingrediente.java
@@ -3,5 +3,21 @@ package proyecto.Objetos;
 
 
 public class Ingrediente {
-   //creacion de la clase ingrediente. 
+  //Creacion de atributos
+    private int id;
+    private String nombre;
+    private boolean agregado;
+    
+  //Creacion del metodo vacio  
+    public Ingrediente(){
+    }
+
+  //Constructor lleno
+    public Ingrediente(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.agregado=false;
+    }
+    
+    
 }
