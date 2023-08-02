c6033b12ecc7d8f1434b9a9a449002336741f5e4
jerem <jerem@LAPTOP-E71OG2F9>
2023-08-01 18:30:49 -0600

    creacion de la clase orden

--- src/proyecto/Objetos/orden.java
+++ src/proyecto/Objetos/orden.java
@@ -1,8 +1,33 @@
 
 package proyecto.Objetos;
 
+//Creacion de la clase orden sus respectivos atributos//
+
+import Estructura_Orden.Pila_ingredienteAgregado;
+
+
 public class orden {
+    //Atributos de la clase orden//
     private int id;
+    private Hamburguesa hamburguesa;
+    private Pila_ingredienteAgregado pila_ingredienteAgregado
+            = new Pila_ingredienteAgregado();
+    private boolean finalizada;
+
+    public orden() {
+    }
+
+    public orden(int id, Hamburguesa hamburguesa) {
+        this.id = id;
+        this.hamburguesa = hamburguesa;
+        this.finalizada = false;
+        
+    }
+
+    
+    
+    
+            
     
            
 }
