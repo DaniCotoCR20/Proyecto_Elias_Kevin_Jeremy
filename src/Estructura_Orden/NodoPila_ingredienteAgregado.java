1ff7ef965b3b8362a6e5d19f77194f2b0938000d
jerem <jerem@LAPTOP-E71OG2F9>
2023-08-01 18:58:40 -0600

    creacion de los gets and setter de la clase NodoPila_ingredienteAgregado

--- src/Estructura_Orden/NodoPila_ingredienteAgregado.java
+++ src/Estructura_Orden/NodoPila_ingredienteAgregado.java
@@ -8,6 +8,28 @@ public class NodoPila_ingredienteAgregado {
     
     private Ingrediente ingrediente;
     private NodoPila_ingredienteAgregado siguiente;
+
+    public NodoPila_ingredienteAgregado(Ingrediente ingrediente, NodoPila_ingredienteAgregado siguiente) {
+        this.ingrediente = null;
+        this.siguiente = null;
+    }
+
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public NodoPila_ingredienteAgregado getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPila_ingredienteAgregado siguiente) {
+        this.siguiente = siguiente;
+    }
+    
     
   
     
