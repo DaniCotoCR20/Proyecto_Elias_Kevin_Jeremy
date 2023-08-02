8f9d442f5dec4788044f26f2a09f5eac96e5adaf
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 19:32:19 -0600

    Creacion de CONSTRUCTOR NodoCola_Orden_Finalizada

--- src/Estructura_Orden/NodoCola_Orden_Finalizada.java
+++ src/Estructura_Orden/NodoCola_Orden_Finalizada.java
@@ -8,4 +8,12 @@ public class NodoCola_Orden_Finalizada {
     //Atributos 
     private orden Orden;
     private NodoCola_Orden_Finalizada atras;
+
+    public NodoCola_Orden_Finalizada(orden Orden, 
+            NodoCola_Orden_Finalizada atras) {
+        this.Orden = Orden;
+        this.atras = atras;
+    }
+    
+    
 }
