eb84ea51d3a8be7d027f8c018a86349719fcab0d
Elías <Elas@DESKTOP-2KETQI6>
2023-08-01 13:03:06 -1200

    Creacion del metodo encola de la clase orden

--- src/Estructura_Orden/Cola_Orden.java
+++ src/Estructura_Orden/Cola_Orden.java
@@ -1,26 +1,37 @@
-
 package Estructura_Orden;
 
-
 public class Cola_Orden {
-  
-     //Creacion de la clase orden y sus atributos//
-    
+
+    //Creacion de la clase orden y sus atributos//
     private NodoCola_Orden frente;
     private NodoCola_Orden ultimo;
     private int largo;
 
     //Metodo que me mujestra el tamanio y el largo de la cola
-    
     public int tamanio() {
         return largo;
     }
 
     //Metodo que me muestra si la cola esta vacia o no//
-    
     public boolean Vacia() {
         return frente == null;
     }
-    
-    
+
+    //Creacion del metodo encolar una orden//
+//     public void encola(Orden orden) {
+//
+//        NodoCola_Orden newNodo = new NodoCola_Orden();
+//        newNodo.setOrden(orden);
+//
+//        if (Vacia()) {
+//            frente = newNodo;
+//            ultimo = newNodo;
+//        } else {
+//            ultimo.setAtras(newNodo);
+//            ultimo = newNodo;
+//        }
+//        largo++;
+//
+//    }
+//    
 }
